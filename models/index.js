25b979dad375f1fd4c4556171841a61f0d4f3cc7
kashyapkumar jivani <kashyap.j@crestinfosystems.net>
2022-10-11 10:33:07 +0530

    added room api calls

--- models/index.js
+++ models/index.js
@@ -3,15 +3,23 @@ const Customers = require('./customers');
 const CustomerLocations = require('./customer_locations');
 const Family = require('./family');
 const Child = require('./child');
+const Camera = require('./camera');
+const RecentViewers = require('./recent_viewers');
+const Room = require('./room');
 
 CustomerLocations.belongsTo(Customers, { foreignKey: 'cust_id' });
-
+Camera.belongsTo(Customers, { foreignKey: 'cust_id' });
+Users.belongsTo(Customers, { foreignKey: 'cust_id' });
 Child.belongsTo(Family, { foreignKey: 'family_id' });
-
+Room.belongsTo(Customers, { foreignKey: 'cust_id' });
+// Room.belongsTo(CustomerLocations, { foreignKey: 'location' });
 module.exports = {
   Users,
   Customers,
   CustomerLocations,
   Family,
-  Child
+  Child,
+  CustomerLocations,
+  RecentViewers,
+  Room
 };
