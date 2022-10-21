ab8f4feb883920ae0edf8d26765d294828a00333
Adil Lightwala <adil.l@crestinfosystems.net>
2022-10-21 07:51:53 +0530

    added the logic to disbale camera selection when limit is reached and created the no camera selected division

--- src/components/watchstream/watchstream.jsx
+++ src/components/watchstream/watchstream.jsx
@@ -1,19 +1,23 @@
 import {
   Autocomplete,
+  Box,
   Card,
   CardContent,
   Chip,
   Grid,
+  Stack,
   // FormControl,
   // InputLabel,
   // MenuItem,
   // Select,
-  TextField
+  TextField,
+  Typography
 } from '@mui/material';
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useContext } from 'react';
 import LayoutContext from '../../context/layoutcontext';
 import AuthContext from '../../context/authcontext';
+import VideoOff from '../../assets/video-off.svg';
 // import ReactPlayer from 'react-player';
 
 const WatchStream = () => {
@@ -24,6 +28,8 @@ const WatchStream = () => {
     room: '',
     cameras: []
   });
+  const [limitReached, setLimitReached] = useState(false);
+
   useEffect(() => {
     layoutCtx.setActive(5);
     layoutCtx.setBreadcrumb(['Watch Stream']);
@@ -35,95 +41,106 @@ const WatchStream = () => {
   const handleRoomChange = (_, value) => {
     setCamerasPayload((prevPayload) => ({ ...prevPayload, room: value }));
   };
+  const onSelect = useCallback((_, values) => {
+    setCamerasPayload((prevPayload) => ({ ...prevPayload, cameras: values }));
+    setLimitReached(values.length >= 2);
+  }, []);
+
+  const checkDisable = useCallback(
+    (option) => limitReached && !camerasPayload.cameras.includes(option),
+    [limitReached, camerasPayload]
+  );
+
+  console.log(camerasPayload);
 
   return (
-    <Card className="empty-content-placeholder">
-      <CardContent>
-        <Grid container spacing={2}>
-          <Grid item md={3} sm={12}>
-            <Autocomplete
-              disableClearable
-              options={authCtx.user.location.accessable_locations}
-              id="location"
-              onChange={handleLocationChange}
-              renderInput={(params) => <TextField {...params} label="Location" />}
-            />
+    <Box>
+      <Card>
+        <CardContent>
+          <Grid container spacing={2}>
+            <Grid item md={3} sm={12}>
+              <Autocomplete
+                disableClearable
+                options={authCtx.user.location.accessable_locations}
+                id="location"
+                onChange={handleLocationChange}
+                renderInput={(params) => <TextField {...params} label="Location" />}
+              />
+            </Grid>
+            <Grid item md={3} sm={12}>
+              <Autocomplete
+                disableClearable
+                noOptionsText={!camerasPayload.location ? 'Select loaction first' : 'No Options'}
+                options={camerasPayload.location ? ['Room 1', 'Room 2'] : []}
+                id="room"
+                onChange={handleRoomChange}
+                renderInput={(params) => <TextField {...params} label="Room" />}
+              />
+            </Grid>
+            <Grid item md={3} sm={12}>
+              <Autocomplete
+                fullWidth
+                multiple
+                id="Camera"
+                noOptionsText={
+                  !camerasPayload.room && !camerasPayload.location
+                    ? 'Select location and room first'
+                    : !camerasPayload.room
+                    ? 'Select room first'
+                    : 'No Camera'
+                }
+                onChange={onSelect}
+                getOptionDisabled={checkDisable}
+                options={!camerasPayload.room ? [] : ['Cam 1', 'Cam 2', 'Cam 3']}
+                renderTags={(value, getTagProps) =>
+                  value.map((option, index) => (
+                    <Chip key={index} label={option} {...getTagProps({ index })} />
+                  ))
+                }
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    label="Cameras"
+                    fullWidth
+                    placeholder="Cameras"
+                    helperText={limitReached && 'Maxmimum two cameras can be selected'}
+                  />
+                )}
+              />
+            </Grid>
           </Grid>
-          <Grid item md={3} sm={12}>
-            <Autocomplete
-              disableClearable
-              noOptionsText={!camerasPayload.location ? 'Select loaction first' : 'No Options'}
-              options={camerasPayload.location ? ['Room 1', 'Room 2'] : []}
-              id="room"
-              onChange={handleRoomChange}
-              renderInput={(params) => <TextField {...params} label="Room" />}
-            />
-          </Grid>
-          <Grid item md={3} sm={12}>
-            <Autocomplete
-              fullWidth
-              multiple
-              id="Camera"
-              noOptionsText={
-                !camerasPayload.room && !camerasPayload.location
-                  ? 'Select location and room first'
-                  : !camerasPayload.room
-                  ? 'Select room first'
-                  : 'No Camera'
-              }
-              options={[]}
-              isOptionEqualToValue={(option, value) => option.camera_id === value.camera_id}
-              getOptionLabel={(option) => {
-                return option.camera_name;
-              }}
-              // onChange={handleRoomChange}
-              renderTags={(value, getTagProps) =>
-                value.map((option, index) => (
-                  <Chip key={index} label={option.room_name} {...getTagProps({ index })} />
-                ))
-              }
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  label="Cameras"
-                  fullWidth
-                  placeholder="Cameras"
-                  InputProps={{
-                    ...params.InputProps,
-                    endAdornment: (
-                      <React.Fragment>
-                        {/* {roomsDropdownLoading ? (
-                                <CircularProgress color="inherit" size={20} />
-                              ) : null}
-                              {params.InputProps.endAdornment} */}
-                      </React.Fragment>
-                    )
-                  }}
-                />
-              )}
-            />
-          </Grid>
-        </Grid>
-        {/* <ReactPlayer
-          url="http://localhost/stream/d2b06a13-4608-46db-a090-0dcf49e54829/index.m3u8"
-          controls={true}
-          config={{
-            file: {
-              hlsOptions: {
-                forceHLS: true,
-                debug: false,
-                xhrSetup: function (xhr) {
-                  xhr.setRequestHeader(
-                    'Authorization',
-                    'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo0MCwiaWF0IjoxNjY2MTYxOTc4LCJleHAiOjE2NjYyNDgzNzh9.uSKW_86tePt2XTL4Rk99szLACfM_rUCGtJ-eiZdgndg'
-                  );
+          <Box mt={2} sx={{ height: '600px' }} className="no-camera-wrapper">
+            <Stack
+              className="no-camera-stack"
+              spacing={1}
+              alignItems="center"
+              justifyContent="center">
+              <img src={VideoOff} />
+              <Typography>Camera not selected</Typography>
+            </Stack>
+            {/* <ReactPlayer
+            url="http://localhost/stream/d7009e03-fdc6-4ca3-a58f-6d9565ba71df/index.m3u8"
+            width={'100%'}
+            controls={true}
+            config={{
+              file: {
+                hlsOptions: {
+                  forceHLS: true,
+                  debug: false,
+                  xhrSetup: function (xhr) {
+                    xhr.setRequestHeader(
+                      'Authorization',
+                      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjozMCwiaWF0IjoxNjY2MjY1ODUxLCJleHAiOjE2NjYzNTIyNTF9.VGFKTIB7MRc4aSdJDEduE0BOZDxm3yyanwMqZXxYX-0'
+                    );
+                  }
                 }
               }
-            }
-          }}
-        /> */}
-      </CardContent>
-    </Card>
+            }}
+          /> */}
+          </Box>
+        </CardContent>
+      </Card>
+    </Box>
   );
 };
 
