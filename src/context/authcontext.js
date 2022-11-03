876805ba8646226e096b1838c4f26cf131b4a521
kashyapkumar jivani <kashyap.j@crestinfosystems.net>
2022-11-03 16:38:28 +0530

    fixed token expired message on login

--- src/context/authcontext.js
+++ src/context/authcontext.js
@@ -5,7 +5,8 @@ const defaultState = {
   auth: {
     token: localStorage.getItem('token'),
     user: JSON.parse(localStorage.getItem('user')),
-    authError: false
+    authError: false,
+    previosPagePath: ''
   }
 };
 
@@ -19,6 +20,7 @@ export const AuthContextProvider = (props) => {
   const [token, setToken] = useState(defaultState.auth.token);
   const [user, setUser] = useState(defaultState.auth.user);
   const [authError, setAuthError] = useState(defaultState.auth.authError);
+  const [previosPagePath, setPreviosPagePath] = useState(defaultState.auth.previosPagePath);
 
   // Method to handle token changes
   const handleToken = (token) => setToken(token);
@@ -29,13 +31,18 @@ export const AuthContextProvider = (props) => {
   // Method to handle authentication status to render invalid token message
   const handleAuthError = (error) => setAuthError(error);
 
+  // Method to handle page change and store previos page path
+  const handlePageChange = (path) => setPreviosPagePath(path);
+
   const context = {
     token,
     user,
     authError,
+    previosPagePath,
     setToken: handleToken,
     setUser: handleUser,
-    setAuthError: handleAuthError
+    setAuthError: handleAuthError,
+    setPreviosPagePath: handlePageChange
   };
 
   return <AuthContext.Provider value={context}>{props.children}</AuthContext.Provider>;
