ef8cf529bd4653b147a92ee7d0268b8d08b985e2
kashyap-crest <kashyap.j@crestinfosystems.net>
2023-04-19 18:53:36 +0530

    Added socket

--- src/app.jsx
+++ src/app.jsx
@@ -15,7 +15,7 @@ import AppRoutes from './routes';
 import { getBuildDate } from './utils/utils';
 import packageJson from '../package.json';
 import withClearCache from './ClearCache';
-
+// import useWebSocket from 'react-use-websocket';
 const MainApp = () => {
   const authCtx = useContext(AuthContext);
   const { enqueueSnackbar } = useSnackbar();
@@ -36,7 +36,47 @@ const MainApp = () => {
   useEffect(() => {
     console.log('build date:', getBuildDate(packageJson.buildDate));
   }, []);
-
+  useEffect(() => {
+    if (authCtx.token) {
+      //console.log('=====', authCtx);
+      //console.log(localStorage.getItem('user'));
+      let { user_id, family_member_id } = JSON.parse(localStorage.getItem('user'));
+      //console.log(user_id, family_member_id);
+      let data = {};
+      if (family_member_id) {
+        data = { family_member_id: family_member_id };
+      } else {
+        data = { user_id: user_id };
+      }
+      //navigate('/dashboard');
+      const webSocket = new WebSocket(
+        //'wss://dhuvtskn6f.execute-api.us-east-1.amazonaws.com/stage'
+        'ws://localhost:3001'
+      );
+      console.log('===== webSocket =====', webSocket);
+      webSocket.onopen = function (e, req, res) {
+        //alert('[open] Connection established');
+        //alert('Sending to server');
+        console.log(e, res, 'opopen===');
+        webSocket.send(JSON.stringify(data));
+        webSocket.onmessage = (event) => {
+          console.log('===========in side === on message', event);
+          let data = JSON.parse(event?.data);
+          if (data?.Data?.display_notification) {
+            enqueueSnackbar(data?.Data?.message, { variant: 'success' });
+          }
+        };
+      };
+      webSocket.onmessage = (event) => {
+        console.log('===========on message', event);
+        let data = JSON.parse(event?.data);
+        if (data?.Data?.display_notification) {
+          enqueueSnackbar(data?.Data?.message, { variant: 'success' });
+        }
+      };
+      //enqueueSnackbar(event?.data?.message, { variant: 'success' });
+    }
+  }, [authCtx.token]);
   return (
     <Routes>
       <Route element={<PublicRoute />}>
