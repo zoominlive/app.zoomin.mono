4a82781e4d072dfc54c950b0ea9d1f6a792b1848
bhargav <bhargav.j@crestinfosystems.net>
2024-04-24 12:26:37 +0530

    optional chanining for props in PF

--- src/components/pf/pf.jsx
+++ src/components/pf/pf.jsx
@@ -6,12 +6,12 @@ export function PF() {
   const authCtx = useContext(AuthContext);
 
   const userInfo = {
-    username: authCtx.user.first_name + '' + authCtx.user.last_name, // REQUIRED - any unique user identifier
-    email: authCtx.user.email,
-    firstname: authCtx.user.first_name,
-    lastname: authCtx.user.last_name,
-    signUpAt: authCtx.user.createdAt,
-    role: authCtx.user.role
+    username: authCtx?.user?.first_name + '' + authCtx?.user?.last_name, // REQUIRED - any unique user identifier
+    email: authCtx?.user?.email,
+    firstname: authCtx?.user?.first_name,
+    lastname: authCtx?.user?.last_name,
+    signUpAt: authCtx?.user?.createdAt,
+    role: authCtx?.user?.role
     // props: { customProp1: 123 } used to pass any other properties
   };
 
