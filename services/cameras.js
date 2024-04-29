5ec7e6bbfba635e7d0722cefb1c23963664f0602
bhargav <bhargav.j@crestinfosystems.net>
2024-04-29 09:40:55 +0530

    modify the query to get only present day records

--- services/cameras.js
+++ services/cameras.js
@@ -185,7 +185,7 @@ module.exports = {
     // )
     let result = await sequelize
       .query(
-        'SELECT recent_user_id, COUNT(DISTINCT sub.viewer_id) AS total_start_only_viewers FROM (SELECT recent_user_id,viewer_id FROM mounted_camera_recent_viewers WHERE `function` = "start" AND cam_id IN (:camIds) GROUP BY recent_user_id) AS sub WHERE NOT EXISTS (SELECT 1 FROM mounted_camera_recent_viewers WHERE `function` = "stop" AND viewer_id = sub.viewer_id)',
+        'SELECT recent_user_id, COUNT(DISTINCT sub.viewer_id) AS total_start_only_viewers FROM (SELECT recent_user_id,viewer_id FROM mounted_camera_recent_viewers WHERE `function` = "start" AND cam_id IN (:camIds) AND DATE(requested_at) = CURDATE() GROUP BY recent_user_id) AS sub WHERE NOT EXISTS (SELECT 1 FROM mounted_camera_recent_viewers WHERE `function` = "stop" AND viewer_id = sub.viewer_id) AND DATE(requested_at) = CURDATE()',
         { replacements: { camIds }, type: sequelize.QueryTypes.SELECT }
       )
 
