1d29b34ac8cff63d739bd56e9ac5df381ab19cfa
bhargav <bhargav.j@crestinfosystems.net>
2025-06-02 12:25:45 +0530

    feat: Add container management endpoints for restarting, updating images, and configuring containers

--- routes/api/agents.js
+++ routes/api/agents.js
@@ -8,6 +8,7 @@ const agentMiddleware = require('../../middleware/agentAuth');
 const agentController = require('../../controllers/agents');
 const { withAuthentication } = require('@frontegg/client');
 const containerMetrics = require('../../controllers/containerMetrics');
+const containerController = require('../../controllers/container');
 
 router.post('/register', agentMiddleware, agentController.createAgent);
 router.put('/update/', agentMiddleware, agentController.updateAgent);
@@ -18,5 +19,11 @@ router.get('/', withAuthentication(), authController, agentController.getAllAgen
 router.get('/:agentId', withAuthentication(), authController, agentController.getAgentById);
 router.post('/bulk-create', containerMetrics.bulkCreateContainerMetrics);
 router.delete('/delete/:agentId', withAuthentication(), authController, agentController.deleteAgent);
+
+// Container management endpoints
+router.post('/restart', containerController.restartContainer);
+router.post('/run-image', containerController.updateContainerImage);
+router.post('/update-config', containerController.updateContainerConfig);
+router.post('/update-muxly-hostname', containerController.updateMuxlyHostname);
 
 module.exports = router;
